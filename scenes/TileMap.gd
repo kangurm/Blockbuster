f1eca6c22dec4ec5b7419322e1aa5332a0fbbd50
Utter Mutter <kooduser@pupil.kood.tech>
2024-05-25 16:34:52 +0300

    blocks with different break precentages

--- scenes/TileMap.gd
+++ scenes/TileMap.gd
@@ -15,24 +15,27 @@ var reach = 15
 var current_tile_pos: Vector2i
 
 func break_block(event, tile_atlas_coord, tile_mouse_pos):
+	var coordX = tile_atlas_coord.x
+	var coordY = tile_atlas_coord.y
 	
-	
-	set_cell(0, tile_mouse_pos, -1)
-	if tile_atlas_coord == 0:
-		block_inv['stone'] += 1
-	if tile_atlas_coord == 1:
-		print('nothing yet')
-	if tile_atlas_coord == 2 :
-		block_inv['dirt'] +=1
-	if tile_atlas_coord == 3:
-		block_inv['granite'] +=1
-	if tile_atlas_coord == 4:
-		block_inv['obsidian'] +=1
-	if tile_atlas_coord == 5:
-		block_inv['oil_shale'] += 1
-	
-	
-	
+	if coordX < 4 && coordX > -1:
+		set_cell(0, tile_mouse_pos,1, Vector2i(coordX+1, coordY))
+	else:
+			
+		if coordY == 4:
+			block_inv['dirt'] += 1
+		if coordY == 3:
+			block_inv['stone'] += 1
+		if coordY == 2 :
+			block_inv['granite'] +=1
+		if coordY == 1:
+			block_inv['obsidian'] +=1
+		if coordY == 0:
+			block_inv['oil_shale'] +=1
+		set_cell(0, tile_mouse_pos, -1)
+
+
+
 
 func mine(event) :
 	await get_tree().create_timer(0.1).timeout
@@ -43,43 +46,39 @@ func mine(event) :
 	var rel_position = mouse_pos-center
 	var distance = local_to_map(rel_position)
 	var tile_mouse_pos = local_to_map(mouse_position)
-	var tile_atlas_coord = get_cell_atlas_coords(0, tile_mouse_pos).y
-	#print(tier)
+	var tile_atlas_coord = get_cell_atlas_coords(0, tile_mouse_pos)
 	if tier == 0:
-		if current_tile_pos != tile_mouse_pos && get_cell_atlas_coords(0, tile_mouse_pos) != Vector2i(9,5) && distance.x <reach && distance.x > -reach && distance.y < reach && distance.y > -reach :
+		if current_tile_pos != tile_mouse_pos && get_cell_atlas_coords(0, tile_mouse_pos) != Vector2i(9,5) && distance.x <reach && distance.x > -reach && distance.y < reach && distance.y > -reach && get_cell_source_id(0, tile_mouse_pos) != 0 :
 			break_block(event, tile_atlas_coord, tile_mouse_pos)
 	if tier == 1:
 		for i in 3:
 			for j in 3:
 				var coord =  Vector2i (tile_mouse_pos.x-1+i, tile_mouse_pos.y-1+j)
-				if current_tile_pos != tile_mouse_pos && get_cell_atlas_coords(0, coord) != Vector2i(9,5) && distance.x <reach && distance.x > -reach && distance.y < reach && distance.y > -reach :
-					tile_atlas_coord = get_cell_atlas_coords(0,coord).y
+				if current_tile_pos != tile_mouse_pos && get_cell_atlas_coords(0, coord) != Vector2i(9,5) && distance.x <reach && distance.x > -reach && distance.y < reach && distance.y > -reach && get_cell_source_id(0, coord) != 0 :
+					tile_atlas_coord = get_cell_atlas_coords(0,coord)
 					break_block(event, tile_atlas_coord, coord)
 	if tier == 2:
 		for i in 5:
 			for j in 5:
 				var coord =  Vector2i (tile_mouse_pos.x-2+i, tile_mouse_pos.y-2+j)
-				if current_tile_pos != tile_mouse_pos && get_cell_atlas_coords(0, coord) != Vector2i(9,5) && distance.x <reach && distance.x > -reach && distance.y < reach && distance.y > -reach :
-					tile_atlas_coord = get_cell_atlas_coords(0,coord).y
+				if current_tile_pos != tile_mouse_pos && get_cell_atlas_coords(0, coord) != Vector2i(9,5) && distance.x <reach && distance.x > -reach && distance.y < reach && distance.y > -reach && get_cell_source_id(0, coord) != 0  :
+					tile_atlas_coord = get_cell_atlas_coords(0,coord)
 					break_block(event, tile_atlas_coord, coord)
 	if tier == 3:
 		for i in 9:
 			for j in 9:
 				var coord =  Vector2i (tile_mouse_pos.x-4+i, tile_mouse_pos.y-4+j)
-				if current_tile_pos != tile_mouse_pos && get_cell_atlas_coords(0, coord) != Vector2i(9,5) && distance.x <reach && distance.x > -reach && distance.y < reach && distance.y > -reach :
-					tile_atlas_coord = get_cell_atlas_coords(0,coord).y
+				if current_tile_pos != tile_mouse_pos && get_cell_atlas_coords(0, coord) != Vector2i(9,5) && distance.x <reach && distance.x > -reach && distance.y < reach && distance.y > -reach && get_cell_source_id(0, coord) != 0  :
+					tile_atlas_coord = get_cell_atlas_coords(0,coord)
 					break_block(event, tile_atlas_coord, coord)
 	if tier == 4:
 		for i in 15:
 			for j in 15:
 				var coord =  Vector2i (tile_mouse_pos.x-7+i, tile_mouse_pos.y-7+j)
-				if current_tile_pos != tile_mouse_pos && get_cell_atlas_coords(0, coord) != Vector2i(9,5) && distance.x <reach && distance.x > -reach && distance.y < reach && distance.y > -reach :
-					tile_atlas_coord = get_cell_atlas_coords(0,coord).y
+				if current_tile_pos != tile_mouse_pos && get_cell_atlas_coords(0, coord) != Vector2i(9,5) && distance.x <reach && distance.x > -reach && distance.y < reach && distance.y > -reach && get_cell_source_id(0, coord) != 0 :
+					tile_atlas_coord = get_cell_atlas_coords(0,coord)
 					break_block(event, tile_atlas_coord, coord)
 	print(block_inv)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
 
 func _input (event):
 	if Input.is_action_just_pressed("mb_left"):
@@ -93,7 +92,7 @@ func _input (event):
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
 			var mouse_position = get_global_mouse_position()
 			var tile_mouse_pos = local_to_map(mouse_position)
-			var source_id = 0
-			var atlas_coord = Vector2i(1,0)
+			var source_id = 1
+			var atlas_coord = Vector2i(0,0)
 			
 			set_cell(0,tile_mouse_pos, source_id, atlas_coord)
--- scenes/player.gd
+++ scenes/player.gd
@@ -17,7 +17,7 @@ func _physics_process(delta):
 	
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += gravity*jump_height * delta
 		#print(velocity.y, "|", gravity, "|", delta)
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
