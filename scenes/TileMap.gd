ed11565d2abeb9f7c56c0b9c39c2d1410eb30817
Utter Mutter <kooduser@pupil.kood.tech>
2024-05-26 00:12:39 +0300

    fixed something

--- scenes/TileMap.gd
+++ scenes/TileMap.gd
@@ -2,17 +2,17 @@ extends TileMap
 
 signal tile_broken
 
-var tier = globals.toolTier
+
 var wait = 0.1
 var reach = 15
 var current_tile_pos: Vector2i
 
 func break_block(event, tile_atlas_coord, tile_mouse_pos):
-	tier = globals.toolTier
+	
 	var coordX = tile_atlas_coord.x
 	var coordY = tile_atlas_coord.y
-	var damage = tier - coordY +1
-	if tier >=coordY:
+	var damage = globals.toolTier - coordY +1
+	if globals.toolTier >=coordY:
 		if coordX + damage < 5 && coordX > -1:
 			set_cell(0, tile_mouse_pos,1, Vector2i(coordX+damage, coordY))
 		else:
@@ -70,31 +70,31 @@ func mine(event):
 	await get_tree().create_timer(wait).timeout
 	
 	
-	if tier == 0:
+	if globals.toolTier == 0:
 		if current_tile_pos != tile_mouse_pos && get_cell_atlas_coords(0, tile_mouse_pos) != Vector2i(9,5) && distance.x <reach && distance.x > -reach && distance.y < reach && distance.y > -reach && get_cell_source_id(0, tile_mouse_pos) != 0 :
 			break_block(event, tile_atlas_coord, tile_mouse_pos)
-	if tier == 1:
+	if globals.toolTier == 1:
 		for i in 3:
 			for j in 3:
 				var coord =  Vector2i (tile_mouse_pos.x-1+i, tile_mouse_pos.y-1+j)
 				if current_tile_pos != tile_mouse_pos && get_cell_atlas_coords(0, coord) != Vector2i(9,5) && distance.x <reach && distance.x > -reach && distance.y < reach && distance.y > -reach && get_cell_source_id(0, coord) != 0 :
 					tile_atlas_coord = get_cell_atlas_coords(0,coord)
 					break_block(event, tile_atlas_coord, coord)
-	if tier == 2:
+	if globals.toolTier == 2:
 		for i in 5:
 			for j in 5:
 				var coord =  Vector2i (tile_mouse_pos.x-2+i, tile_mouse_pos.y-2+j)
 				if current_tile_pos != tile_mouse_pos && get_cell_atlas_coords(0, coord) != Vector2i(9,5) && distance.x <reach && distance.x > -reach && distance.y < reach && distance.y > -reach && get_cell_source_id(0, coord) != 0  :
 					tile_atlas_coord = get_cell_atlas_coords(0,coord)
 					break_block(event, tile_atlas_coord, coord)
-	if tier == 3:
+	if globals.toolTier == 3:
 		for i in 9:
 			for j in 9:
 				var coord =  Vector2i (tile_mouse_pos.x-4+i, tile_mouse_pos.y-4+j)
 				if current_tile_pos != tile_mouse_pos && get_cell_atlas_coords(0, coord) != Vector2i(9,5) && distance.x <reach && distance.x > -reach && distance.y < reach && distance.y > -reach && get_cell_source_id(0, coord) != 0  :
 					tile_atlas_coord = get_cell_atlas_coords(0,coord)
 					break_block(event, tile_atlas_coord, coord)
-	if tier == 4:
+	if globals.toolTier == 4:
 		for i in 15:
 			for j in 15:
 				var coord =  Vector2i (tile_mouse_pos.x-7+i, tile_mouse_pos.y-7+j)
@@ -110,10 +110,10 @@ func _input (event):
 			wait += 0.1
 			print('breaker globals.toolTier:', globals.toolTier)
 	if Input.is_action_just_pressed("pressJ"):
-		if tier >0:
-			tier -=1
+		if globals.toolTier >0:
+			globals.toolTier -=1
 			wait -= 0.1
-			print('breaker tier:', tier)
+			print('breaker globals.toolTier', globals.toolTier)
 	if Input.is_action_just_pressed("mb_left"):
 		while true:
 			if(Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)):
