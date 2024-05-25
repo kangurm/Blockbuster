5c6ffe0be419ab3ddbffda12ae9a8ac497021889
Utter Mutter <kooduser@pupil.kood.tech>
2024-05-25 17:54:04 +0300

    s

---
+++ Player/HUD/HUD.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+#func _input(event):
+	#if Input.is_action_pressed('enter'):
+		#var tilemap = get_node('../../TileMap')
+		#
+		#print('enter', tilemap.block_inv)
