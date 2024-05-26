a503c4a396c62a255133b422290befcf8881d0b3
Marcus Kangur <kangur.marcus@gmail.com>
2024-05-26 16:13:01 +0300

    Scene name

--- menu.gd
+++ menu.gd
@@ -4,7 +4,7 @@ extends Control
 
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://scenes/grid-test.tscn")
+	get_tree().change_scene_to_file("res://scenes/KivioliBlockbusters.tscn")
 
 
 func _on_quit_pressed():
