5757d55efee263d820f7798c5227cc064ec06e7c
lhaavel <lukas.haavel@gmail.com>
2024-05-26 12:16:24 +0300

    deleted montan

--- scenes/TileMap.gd
+++ scenes/TileMap.gd
@@ -72,7 +72,6 @@ func mine(event):
 	
 	
 	if globals.toolTier == 0:
-		print("break tier 0")
 		if current_tile_pos != tile_mouse_pos && get_cell_atlas_coords(0, tile_mouse_pos) != Vector2i(9,5) && distance.x <reach && distance.x > -reach && distance.y < reach && distance.y > -reach && get_cell_source_id(0, tile_mouse_pos) != 0 :
 			break_block(event, tile_atlas_coord, tile_mouse_pos)
 	if globals.toolTier == 1:
