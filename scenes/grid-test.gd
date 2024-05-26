89ce4338cd77b987be22aa07d28cb616276bab1f
lhaavel <lukas.haavel@gmail.com>
2024-05-26 11:02:45 +0300

    reset globals upon play again

--- scenes/grid-test.gd
+++ scenes/grid-test.gd
@@ -7,6 +7,24 @@ extends Node2D
 var score = 0
 
 func _ready():
+	#reset globals
+	globals.refinery_inv = {
+	"dirt": 0,
+	"stone": 0,
+	"granite": 0,
+	"obsidian": 0,
+	"oil_shale": 0,
+}
+	globals.block_inv = {
+		"dirt": 0,
+		"stone": 0,
+		"granite": 0,
+		"obsidian": 0,
+		"oil_shale": 0,
+}
+	globals.toolTier = 0
+	
+	#connect signals
 	tile_map.tile_broken.connect(player._on_tile_broken)
 	tile_map.tile_broken.connect(hud._update_inventory_UI)
 	#refinery.transfered.connect(hud._update_inventory_UI)
